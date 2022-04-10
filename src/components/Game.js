10c7d702f43c8e6fbed9cbf5a2991b18fd540a38
waywayfox <appiedavid777@gmail.com>
2022-04-10 22:35:05 +0800

    finish basic sps and buying

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,6 +14,7 @@ class Game extends React.Component {
       subscriberPerSecond: 0,
       buildingList: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
+    this.baseSps = [0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   }
 
 
@@ -29,10 +30,21 @@ class Game extends React.Component {
     }))
   }
 
+  calculateBaseSps = () => {
+    for (let i = 1; i < BUILDING_MAX; i += 1)
+    {
+      let id = i;
+      let base = Math.ceil((Math.pow(id * 1, id * 0.5 + 2)) * 10) / 10;
+      let digits = Math.pow(10, (Math.ceil(Math.log(Math.ceil(base)) / Math.LN10))) / 100;
+      base = Math.round(base / digits) * digits;
+      this.baseSps[i] = base;
+    }
+  }
+
   updateSPS = (count) => {
     let sps = 0;
     this.state.buildingList.forEach((count, index) => {
-      sps += ( index + 1 ) * count
+      sps += this.baseSps[index] * count
     });
     console.log(sps);
     this.setState({
@@ -40,22 +52,32 @@ class Game extends React.Component {
     });
   }
 
-  setBuildingAmount = (index, count) => {
+
+  /**
+   * Description set buildingList and add cost to totalSubscriber
+   *
+   * @param {integer} index           index of buildingList.
+   * @param {integer} count           amount of user buy or sell(negative).
+   * @param {integer} cost            cost of buying or earn of selling.
+   */
+  setBuildingAmount = (index, count, cost) => {
     const newList = this.state.buildingList.slice(0);
+    let newTotal = count > 0? this.state.totalSubscriber - cost: this.state.totalSubscriber + cost;
     newList[index] += count;
     if (newList[index] < 0)
     {
       newList[index] = 0;
     }
+
     this.setState({
-      buildingList: newList
+      buildingList: newList,
+      totalSubscriber: newTotal
     }, this.updateSPS);
   }
 
-
-
   componentDidMount() {
     setInterval(this.updateSubscriber, 1000);
+    this.calculateBaseSps();
   }
 
 
@@ -66,7 +88,7 @@ class Game extends React.Component {
         <Row wrap={false}>
           {/**Left part: clicking part and show current subscriber**/}
           <Col span={8}>
-            <h1>{this.state.totalSubscriber}</h1>
+            <h1>{Math.floor( this.state.totalSubscriber )}</h1>
             <ClickingWindow addTotolSubscriber={this.addTotolSubscriber} />
           </Col>
           {/**Middle part: news, some buttons, and show the image for every upgrade**/}
@@ -75,7 +97,7 @@ class Game extends React.Component {
           </Col>
           {/**Right part: upgrade pannel**/}
           <Col span={8}>
-            <PurchasePanel buildingList={this.state.buildingList} setBuildingAmount={this.setBuildingAmount} />
+            <PurchasePanel totalSubscriber={this.state.totalSubscriber} buildingList={this.state.buildingList} setBuildingAmount={this.setBuildingAmount} />
           </Col>
         </Row>
       </>
--- src/components/PurchasePanel.js
+++ src/components/PurchasePanel.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Button, Radio, Space, Row } from 'antd';
+import {BUILDING_MAX} from './../const.js'
 
 
 const PurchaseMode = {
@@ -97,7 +98,69 @@ class PurchasePanel extends Component {
       purchaseMode: PurchaseMode.PURCHASE,
       amountMode: AmountMode.ONE
     }
+    this.basePriceList = [15, 100, 500, 10000, 3000,
+                          0, 0, 0, 40000, 200000,
+                          1666666, 123456789, 3999999999, 75000000000, 77777777777,
+                          12345678987654321, 12345678987654321, 12345678987654321];
+
   }
+
+  componentDidMount() {
+    this.calculateBasePrice();
+  }
+
+  calculateBasePrice = () => {
+
+    for (let i = 1; i < BUILDING_MAX; i += 1)
+    {
+      let id = i;
+      let base = (id * 1 + 9 + (id < 5? 0 : Math.pow(id-5, 1.75) * 5 )) * Math.pow(10, id) * (Math.max(1, id - 14));
+      let digits = Math.pow(10, (Math.ceil(Math.log(Math.ceil(base)) / Math.LN10))) / 100;
+      base = Math.round(base / digits) * digits;
+      if (id >= 16) base *= 10;
+      if (id >= 17) base *= 10;
+      if (id >= 18) base *= 10;
+      if (id >= 19) base *= 10;
+      this.basePriceList[i] = base;
+    }
+
+  }
+
+
+  calculatePrice = (index, amount) => {
+    let price = 0;
+    let base = this.basePriceList[index];
+    let start = this.props.buildingList[index];
+    for (let i = start; i < start + amount; i += 1)
+    {
+      price += base * Math.pow(1.15, i);
+    }
+    return Math.ceil(price);
+
+  }
+
+  calculateSalePrice = (index, amount) => {
+
+    return 0;
+  }
+
+  convertPrice = (price) => {
+    if (price < 1000000)
+    {
+      return String(price);
+    }
+    else
+    {
+      return price.toExponential(3);
+    }
+
+  }
+
+
+  // click process
+  // (1) check how many amount can it actually buy or sale
+  // (2) update the state from Data: totalSubscriber and buildingList
+  //
 
   handlePurchaseModeChange = (e) => {
     this.setState({ purchaseMode: e.target.value });
@@ -111,18 +174,67 @@ class PurchasePanel extends Component {
     this.setState({ amountMode: e.target.value });
   }
 
+
+  /**
+   * Description calculate buy result
+   *
+   * @param {integer} index           index of buildingList.
+   * @return {object} Return a object with amount and cost.
+   */
+  calculateBuyResult = (index) => {
+    const start = this.props.buildingList[index];
+    const target = this.state.amountMode;
+    let currentSubs = this.props.totalSubscriber;
+    let amount = 0;
+    let cost = 0;
+    let base = this.basePriceList[index];
+    console.log(currentSubs);
+    for (let i = start; i < start + target; i += 1)
+    {
+      const currentPrice = base * Math.pow(1.15, i);
+      if (currentSubs >= currentPrice)
+      {
+        amount += 1
+        cost += currentPrice;
+        currentSubs -= currentPrice;
+      }
+      else
+      {
+        break;
+      }
+    }
+    return [amount, cost];
+  }
+
+  /**
+   * Description calculate sell result
+   *
+   * @param {integer} index           index of buildingList.
+   * @return {object} Return a object with amount and earn.
+   */
+  calculateSellResult = (index) => {
+
+    return [0, 0];
+  }
+
+  /**
+   * Description handle buy and sell
+   */
   handleButtonClick = (e) => {
     const id = e.currentTarget.id;
-    const amount = this.state.purchaseMode * this.state.amountMode;
-    this.props.setBuildingAmount(id, amount);
+    const clickResult = this.state.purchaseMode === PurchaseMode.PURCHASE? this.calculateBuyResult(id) : this.calculateSellResult(id);
+    console.log(clickResult);
+    this.props.setBuildingAmount(id, clickResult[0], clickResult[1]);
   }
 
 
   render() {
     const buttons = BuildingsName.map((item, i) => {
+      let price = this.state.purchaseMode == PurchaseMode.PURCHASE ? this.calculatePrice(item.id, this.state.amountMode) : this.calculateSalePrice();
+      let priceString = this.convertPrice(price);
       return (
         <Button id={item.id} key={item.id} block onClick={this.handleButtonClick}>
-          { item.name + ": " + String(this.props.buildingList[item.id])}
+          { item.name + ": " + priceString + " " + String(this.props.buildingList[item.id])}
         </Button>
       )
     });
